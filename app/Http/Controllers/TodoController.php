51b9bdfe7496e7766e49cad4d7a7be2324b96ee9
KOKI <coatchtech0613@gmail.com>
2021-12-19 13:53:20 +0900

    修正

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -18,13 +18,19 @@ class TodoController extends Controller
     {
         $this->validate($request, Todo::$rules);
         $form = $request->all();
+
         Todo::create($form);
         return redirect('/');
     }
 
     public function update(Request $request)
     {
-        
+        $this->validate($request, Todo::$rules);
+        $form = $request->all();
+        unset($form['_token']);
+
+        Todo::where('id', $request->id)->update($form);
+        return redirect('/');
     }
 
     public function delete(Request $request)
